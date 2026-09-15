aabfe0c6406fe87634221765cd3f0b363f67080e
Junaco <shijianzongyi@gmail.com>
2026-09-15 15:46:07 +0900

    Update script.js

--- inabaa/script.js
+++ inabaa/script.js
@@ -171,7 +171,7 @@ judgebtn.addEventListener('click', () => {
   const share_twitter = document.getElementById("js-share-twitter");
   share_twitter.setAttribute(
 	  "href",
-	  "https://twitter.com/share?url=" + "https://shijianzongyi-lang.github.io/inabaa/" + "&text=" + share_title + "&hashtags=いなばあキャラ診断"
+	  "https://twitter.com/share?url=" + "https://wanwan0923.jp/inabaa/" + "&text=" + share_title + "&hashtags=いなばあキャラ診断"
   );
 });
 
